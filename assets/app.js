cf16037d499b1f8044cbbd61195791202516fa29
chado <a.sale@outlook.fr>
2023-06-28 15:44:38 +0200

    update package

--- assets/app.js
+++ assets/app.js
@@ -17,5 +17,5 @@ $(document).ready(function() {
     $('[data-toggle="popover"]').popover();
 });
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+// any CSS you import will output into a single css file (app.scss in this case)
+import './styles/app.scss';
