d9c67eebe21683efef074dc0dcf280060347550c
eric-pat <97623282+eric-pat@users.noreply.github.com>
2023-06-28 22:27:16 +0200

    end navbar

--- assets/app.js
+++ assets/app.js
@@ -13,9 +13,5 @@ require('bootstrap');
 // require('bootstrap/js/dist/tooltip');
 // require('bootstrap/js/dist/popover');
 
-$(document).ready(function() {
-    $('[data-toggle="popover"]').popover();
-});
-
 // any CSS you import will output into a single css file (app.scss in this case)
 import './styles/app.scss';
