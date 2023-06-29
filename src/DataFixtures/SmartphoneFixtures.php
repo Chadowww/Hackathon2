40b22e46d41f85896672c13a8940328e86662b51
chado <a.sale@outlook.fr>
2023-06-29 17:28:47 +0200

    fix: service for evaluate category price

--- src/DataFixtures/SmartphoneFixtures.php
+++ src/DataFixtures/SmartphoneFixtures.php
@@ -10,6 +10,8 @@ use Faker\Factory;
 
 class SmartphoneFixtures extends Fixture implements DependentFixtureInterface
 {
+    const MEMORY =[2,4,5,8,10,12,24,];
+    const CONDITIONS = ['A vécu', 'Êtat correct', 'Bon état', 'Parfait état'];
     const SMARTPHONES = [
         [
             'is_sold' => 0,
@@ -45,10 +47,12 @@ class SmartphoneFixtures extends Fixture implements DependentFixtureInterface
             $faker = Factory::create('fr_FR');
             $smartphoneForFixture = new Smartphone();
             $smartphoneForFixture ->setmodel($faker->word);
+            $smartphoneForFixture ->setPhoneCondition($this->getReference('phone_condition_' . array_rand(array_flip
+                (self::CONDITIONS))));
             $smartphoneForFixture ->setHasCharger($smartphone ['has_charger']);
             $smartphoneForFixture ->setIsSold($smartphone ['is_sold']);
             $smartphoneForFixture ->setCategory($this->getReference('category_' . $smartphone['category_code']));
-            $smartphoneForFixture ->setMemory($this->getReference('memory_' . $i));
+            $smartphoneForFixture ->setMemory(array_rand(array_flip(self::MEMORY)));
             $smartphoneForFixture ->setStorage($this->getReference('storage_' . $i));
             $smartphoneForFixture->setReleaseDate($faker->dateTimeBetween('-10 years', 'now'));
             $smartphoneForFixture ->setCreatedAt(new \DateTime());
--- src/Services/CategoryCalculatorService.php
+++ src/Services/CategoryCalculatorService.php
@@ -4,8 +4,10 @@ namespace App\Services;
 
 use App\Entity\Memory;
 use App\Entity\PhoneAge;
+use App\Entity\PhoneCondition;
 use App\Entity\Storage;
 use App\Repository\CategoryRepository;
+use App\Repository\MemoryRepository;
 use App\Repository\SmartphoneRepository;
 use App\Repository\YearRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,13 +19,15 @@ class CategoryCalculatorService
     public Storage $storage;
     public Memory $memory;
     public PhoneAge $age;
+    public PhoneCondition $phoneCondition;
 
     public function __construct(
         SmartphoneRepository $smartphoneRepository,
         APIServices $apiServices,
         YearRepository $phoneAge,
         CategoryRepository $categoryRepository,
-        EntityManagerInterface $manager
+        EntityManagerInterface $manager,
+        MemoryRepository $memoryRepository,
     )
     {
         $this->smartphoneRepository = $smartphoneRepository;
@@ -32,37 +36,46 @@ class CategoryCalculatorService
         $this->memory = new Memory();
         $this->storage = new Storage();
         $this->age = new PhoneAge();
+        $this->phoneCondition = new PhoneCondition();
         $this->categoryRepository = $categoryRepository;
         $this->manager = $manager;
+        $this->memoryRepository = $memoryRepository;
     }
 
     public function calculateCategory($id)
     {
         $smartphones = $this->smartphoneRepository->findBy(['id' => $id]);
+        $categories = $this->categoryRepository->findAll();
 
-        $category = $this->categoryRepository->findAll();
+        $yearRelease = $smartphones[0]->getReleaseDate();
+        $yearRelease = $yearRelease->format('Y');
+        $currentlyYear = date('Y');
+        $phoneAge = $currentlyYear - $yearRelease;
 
-        $year = $smartphones[0]->getReleaseDate();
-        $year = $year->format('Y');
-        $yearNow = date('Y');
-        $phoneAge = $yearNow - $year;
-
-        $storage = $smartphones[0]->getStorage();
-        $ram = $smartphones[0]->getMemory();
         $age = $this->phoneAge->findBy(['phoneAge' => $phoneAge]);
 
-        $valueStorage = $storage->getValueStorage();
-        $valueRam = $ram->getValueMemory();
+        $ram = $smartphones[0]->getMemory();
+        $valueRam = $this->memoryRepository->findBy(['ramNumber' => $ram]);
+
+
+        $condition = $smartphones[0]->getPhoneCondition();
+
+        $storage = $smartphones[0]->getStorage();
+
         $valueAge = $age[0]->getValueYear();
+        $valueRam = $valueRam[0]->getValueMemory();
+        $valueStorage = $storage->getValueStorage();
 
-        $totalValue = $valueStorage + $valueRam + $valueAge;
+        $totalValue = ($valueStorage + $valueRam + $valueAge) * $condition->getPriceDepreciation();
 
-        foreach ($category as $key => $value) {
+        foreach ($categories as $key => $value) {
             if ($totalValue >= $value->getIndiceMin() && $totalValue <= $value->getIndiceMax()) {
                 $smartphones[0]->setCategory($value);
                 $this->manager->persist($smartphones[0]);
                 $this->manager->flush();
             }
         }
+
+        return $totalValue;
     }
 }
